13ba93401c187c33f38713beedcf87a1d747b92c
hassan samy <32715535+HSamy12@users.noreply.github.com>
2022-02-13 20:51:35 +0200

    adding the second part of lab2

---
+++ Embedded C/3.3/lab2_part2/main.c
@@ -0,0 +1,31 @@
+/*
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Hassan Samy
+ * @brief          : toggle led program
+ ******************************************************************************
+ */
+#include "Plat_type.h"
+
+#define Base_RCC 	0x40021000
+#define GPIO_PORTA 	0x40010800
+#define RCC_ABP2ENR *(v_uint32 *)(Base_RCC + 0x18)
+#define GPIO_CRH	*(v_uint32 *)(GPIO_PORTA + 0x04)
+#define GPIO_ORD	*(v_uint32 *)(GPIO_PORTA + 0x0C)
+
+uint8 g_variables[3] ={1,2,3};
+uint8 const const_variables[3] ={1,2,3};
+
+void main(void){
+	RCC_ABP2ENR |= (1<<2);
+	GPIO_CRH &= 0xFF0FFFFF;
+	GPIO_CRH |= 0x00200000;
+	uint32 i;
+	while(1){
+		GPIO_ORD |=(1<<13);
+		for(i =0;i<5000;i++);
+
+		GPIO_ORD &=~(1UL<<13);
+		for(i = 0;i<5000;i++);
+	}
+}
